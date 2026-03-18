7e1fdb53daeee6e5ca72f11ab3030f9e3e18bae7
damikun <ikundam654@gmail.com>
2026-03-18 15:55:01 +0800

    chore: 初始化Modern-AI 项目基础代码

---
+++ src/main/java/com/teamdev/modernai/ModernAiApplication.java
@@ -0,0 +1,13 @@
+package com.teamdev.modernai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamdev/modernai/ModernAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamdev.modernai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

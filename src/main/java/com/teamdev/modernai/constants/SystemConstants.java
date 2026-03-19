b5c8bb8492e07be0f006085f170e40c2439e5b3c
Saber <3322155084@qq.com>
2026-03-19 10:06:29 +0800

    实现底层的自定义大模型接入与全局Bean

---
+++ src/main/java/com/teamdev/modernai/constants/SystemConstants.java
@@ -0,0 +1,7 @@
+package com.teamdev.modernai.constants;
+
+public class SystemConstants {
+    public static final String GAME_SYSTEM_PROMPT = "";
+
+    public static final String SERVICE_SYSTEM_PROMPT = "";
+}
